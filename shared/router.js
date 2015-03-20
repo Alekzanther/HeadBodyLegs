b9a73b5c35c7da08ef692fd1e4580170d16faf68
Simon <=>
2015-03-20 21:11:54 +0100

    Added myPictures, pictures are unique on id instead of name

--- shared/router.js
+++ shared/router.js
@@ -6,6 +6,15 @@ Router.route('/draw', function () {
   this.render('canvas');
 });
 
+Router.route('/draw/:_id', function () {
+  var item = Pictures.findOne({_id: this.params._id});
+  this.render('canvas', {data: {testPic : item}});
+});
+
 Router.route('/montage', function () {
   this.render('montage');
+});
+
+Router.route('/mypictures', function () {
+  this.render('myPictures');
 });
